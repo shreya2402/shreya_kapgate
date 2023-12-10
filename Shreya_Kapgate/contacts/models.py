7627a8968e6c51ca375d9c182dece0248217b1b3
Shreya Kapgate <shreyakapgate24@gmail.com>
2023-12-10 14:07:35 -0800

    Field restrictions and icons commit

--- Shreya_Kapgate/contacts/models.py
+++ Shreya_Kapgate/contacts/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 # Create your models here.
 class Contact(models.Model):
-    name = models.CharField(max_length=20, null=False)
-    email = models.EmailField()
+    name = models.CharField(max_length=20, null=False, unique=True)
+    email = models.EmailField(unique=True)
     created_time = models.DateTimeField(auto_now_add=True, editable=False)
     notes = models.TextField(max_length=200, default="")
