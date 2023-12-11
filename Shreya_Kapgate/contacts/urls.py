642d60acebf41799351ad3199cb6e9f51863ef35
Shreya Kapgate <shreyakapgate24@gmail.com>
2023-12-10 16:40:49 -0800

    record delete commit

--- Shreya_Kapgate/contacts/urls.py
+++ Shreya_Kapgate/contacts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ContactListView, create_contact, ContactDetailView, ContactUpdateView  # Import the create_contact view
+from .views import ContactListView, CreateContactView, ContactDetailView, ContactUpdateView, ContactDeleteView  # Import the create_contact view
 
 urlpatterns = [
     path("", ContactListView.as_view(), name="contact_list"),
-    path("create/", create_contact, name="contact_form"),  # Add the path for creating contacts
+    path("create/", CreateContactView.as_view(), name="contact_create"),
     path("contacts/<int:pk>/", ContactDetailView.as_view(), name="contact_detail"),
     path("contacts/<int:pk>/update", ContactUpdateView.as_view(), name="contact_update"),
+    path("contacts/<int:pk>/delete", ContactDeleteView.as_view(), name="contact_delete"),
 ]
--- Shreya_Kapgate/contacts/views.py
+++ Shreya_Kapgate/contacts/views.py
@@ -1,23 +1,35 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, UpdateView
-from django.views.generic import ListView, DetailView
+from django.views.generic import View, ListView, DetailView, UpdateView, DeleteView
 from .models import Contact
 from .forms import ContactForm
 
-    
 class ContactListView(ListView):
     model = Contact
-def create_contact(request):
-    if request.method == 'POST':
+
+class CreateContactView(View):
+    template_name = 'contacts/contact_form.html'
+
+    def get(self, request):
+        form = ContactForm()
+        context = {
+            'form': form,
+            'is_creation': True  # Pass the variable to the template
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
         form = ContactForm(request.POST)
         if form.is_valid():
-            form.save()  # This will create a new Contact instance
+            form.save()
             return redirect('contact_list')  # Redirect to the contact list page or any other page
-    else:
-        form = ContactForm()
+        return render(request, self.template_name, {'form': form})
 
-    return render(request, 'contacts/contact_form.html', {'form': form})
+class ContactUpdateView(UpdateView):
+    model = Contact
+    form_class = ContactForm
+    template_name = 'contacts/contact_form.html'  # Use the same form template for update
+    success_url = reverse_lazy("contact_list")
 
 class ContactDetailView(DetailView):
     model = Contact
@@ -25,4 +37,15 @@ class ContactDetailView(DetailView):
 class ContactUpdateView(UpdateView):
     model = Contact
     form_class = ContactForm
+    success_url = reverse_lazy("contact_list")
+    template_name = 'contacts/contact_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Pass an additional variable to the template
+        context['is_creation'] = False  # Set it to True for update, False for create
+        return context
+
+class ContactDeleteView(DeleteView):
+    model = Contact
     success_url = reverse_lazy("contact_list")
